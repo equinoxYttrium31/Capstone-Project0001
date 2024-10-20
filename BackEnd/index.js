f8387560df680ed09aa08fda287c24b7cbc43e96
Equinox <johnmarkflameno@gmail.com>
2024-10-20 16:27:53 +0800

    Update index.js

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -27,7 +27,7 @@ app.use(express.urlencoded({ extended: false, limit: "10mb" }));
 // CORS configuration
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["https://client-2oru.onrender.com", "http://localhost:5174"],
     credentials: true, // Allow cookies to be sent and received
   })
 );
