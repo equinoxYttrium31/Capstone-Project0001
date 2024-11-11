2d6145bd49af6dd55af096629039c9a680dfd03f
Equinox <johnmarkflameno@gmail.com>
2024-11-12 01:16:15 +0800

    updated again

--- CBC Hagonoy System Admin/src/pages/announcement_list/EditAnnouncementModal.jsx
+++ CBC Hagonoy System Admin/src/pages/announcement_list/EditAnnouncementModal.jsx
@@ -1,12 +1,20 @@
+import "./Edit_Announcement.css";
 import { updates_placeholder } from "../../assets/Images";
-import "./Announcement_List.css";
 
-<div className="placeholder_container">
-  <img
-    src={updates_placeholder}
-    alt="Update Underway"
-    className="placeholder"
-  />
-  <h1 className="update_header">This part is still under development.</h1>
-  <p className="update_subtext">This may take a while comeback later.</p>
-</div>;
+function EditAnnouncementModal() {
+  return (
+    <div className="edit_announcement_modal">
+      <div className="placeholder_container">
+        <img
+          src={updates_placeholder}
+          alt="Update Underway"
+          className="placeholder"
+        />
+        <h1 className="update_header">This part is still under development.</h1>
+        <p className="update_subtext">This may take a while comeback later.</p>
+      </div>
+    </div>
+  );
+}
+
+export default EditAnnouncementModal;
